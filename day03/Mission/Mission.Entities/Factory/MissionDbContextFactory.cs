b71d6d2fa99789f1d8984dab3bd473d6c077390e
Vinit-0 <vinit7maheshwari@gmail.com>
2025-07-07 13:13:49 +0530

    Day03

---
+++ day03/Mission/Mission.Entities/Factory/MissionDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Entities
+{
+    public class MissionDbContextFactory : IDesignTimeDbContextFactory<MissionDbContext>
+    {
+        public MissionDbContext CreateDbContext(string[] args)
+        {
+            // Set base path to the API project's directory
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Mission.Api"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<MissionDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString); // Or UseNpgsql/UseSqlite/etc. based on your DB
+
+            return new MissionDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ day03/Mission/Mission.Entities/MissionDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Entities
+{
+    public class MissionDbContext(DbContextOptions<MissionDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "Admin",
+                LastName = "Tatva",
+                EmailAddress = "Admin@gmail.com",
+                Password = "Admin@123",
+                PhoneNumber = "123-456-7890",
+                UserType = "Admin",
+                UserImage = null
+
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
