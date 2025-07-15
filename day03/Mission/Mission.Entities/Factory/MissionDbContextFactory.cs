283ca4aab8b2eb023c7c7d71fdff15ee3fe52cd8
Vinit-0 <vinit7maheshwari@gmail.com>
2025-07-15 18:48:52 +0530

    Final changes

--- day03/Mission/Mission.Entities/Factory/MissionDbContextFactory.cs
+++ day03/Mission/Mission.Entities/Factory/MissionDbContextFactory.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mission.Entities
+namespace Mission.Entities.Factory
 {
     public class MissionDbContextFactory : IDesignTimeDbContextFactory<MissionDbContext>
     {
